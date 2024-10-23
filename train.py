0649c717cd9670c9d0840e9ace2d2362aef8e23f
SandaruwanWGMA <sandaruwanwgma@gmail.com>
2024-10-23 09:09:51 +0530

    training 06

--- train.py
+++ train.py
@@ -53,8 +53,7 @@ def main():
 
         for i, data in enumerate(train_loader, 0):
             epoch_iter += 1
-            # low_res_images, high_res_images = data[0][i], data[1][i]
-            low_res_images, high_res_images = data[0], data[1]
+            low_res_images, high_res_images = data[0][i], data[1][i]
 
             if high_res_images.shape[2:] != low_res_images.shape[2:]:
                 print(
